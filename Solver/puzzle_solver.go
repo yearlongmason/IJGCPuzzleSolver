62e9fb9acb3fa2f7334364494f3b8c865a56e8bc
yearlongmason <mdudelee@gmail.com>
2025-12-27 03:31:34 -0500

    Implemented load puzzle functionality

--- Solver/puzzle_solver.go
+++ Solver/puzzle_solver.go
@@ -2,12 +2,50 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"slices"
+	"strings"
 )
 
 type PuzzleState struct {
 	rows, columns int
 	state         [][]string
+}
+
+func stringToPuzzleState(stringPuzzleState string) PuzzleState {
+	// Create the variables needed to make an instance of PuzzleState
+	slicePuzzleState := make([][]string, 0)
+	numRows := len(strings.Split(stringPuzzleState, "\n"))
+	numCols := len(strings.TrimSpace(strings.Split(stringPuzzleState, "\n")[0]))
+
+	// Loop through each row in the puzzle input
+	for row := range strings.SplitSeq(stringPuzzleState, "\n") {
+		// Create a new row to keep track of
+		row = strings.TrimSpace(row)
+		currentRow := make([]string, 0)
+
+		// Add each character to the new row
+		for i := 0; i < len(row); i++ {
+			currentRow = append(currentRow, string(row[i]))
+		}
+
+		// Add the new row to the puzzle state
+		slicePuzzleState = append(slicePuzzleState, currentRow)
+	}
+
+	// Create and return a new PuzzleState
+	newPuzzleState := PuzzleState{numRows, numCols, slicePuzzleState}
+	return newPuzzleState
+}
+
+func loadPuzzle(fileName string) PuzzleState {
+	// Load a puzzle from a file and return the puzzle state
+	stringPuzzleState, err := os.ReadFile("../Puzzles/" + fileName)
+	if err != nil {
+		fmt.Printf("Error, could not read file: %s\n", fileName)
+	}
+
+	return stringToPuzzleState(string(stringPuzzleState))
 }
 
 func puzzleStatesEqual(state1 PuzzleState, state2 PuzzleState) bool {
@@ -28,5 +66,5 @@ func puzzleStatesEqual(state1 PuzzleState, state2 PuzzleState) bool {
 }
 
 func main() {
-	fmt.Println(!slices.Equal([]string{"1", "0", "3"}, []string{"1", "2", "3"}))
+	fmt.Println(loadPuzzle("GizehPuzzle.txt"))
 }
--- Solver/solver_test.go
+++ Solver/solver_test.go
@@ -13,13 +13,8 @@ func TestLoadPuzzle(t *testing.T) {
 			{"0", "0", "0"}},
 	}
 
-	// Actual value to be replaced by a loadPuzzle function
-	actual := PuzzleState{3, 3,
-		[][]string{
-			{"0", "0", "0"},
-			{"0", "0", "0"},
-			{"0", "0", "0"}},
-	}
+	// Get the actual puzzle from the 3x3Puzzle.txt test file
+	actual := loadPuzzle("3x3Puzzle.txt")
 
 	// If the puzzle states are not equal, then raise an error
 	if !puzzleStatesEqual(expected, actual) {
