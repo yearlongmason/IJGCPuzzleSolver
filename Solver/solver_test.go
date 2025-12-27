ed443f94f5a29407528753b07267926704faeeef
yearlongmason <mdudelee@gmail.com>
2025-12-27 15:48:39 -0500

    Added 2x2 test puzzle input for easier getSuccessors test

--- Solver/solver_test.go
+++ Solver/solver_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestLoadPuzzle(t *testing.T) {
+func TestLoadPuzzle3x3(t *testing.T) {
 	// Hardcoded expected value
 	expected := PuzzleState{3, 3,
 		[][]string{
@@ -15,6 +15,23 @@ func TestLoadPuzzle(t *testing.T) {
 
 	// Get the actual puzzle from the 3x3Puzzle.txt test file
 	actual := loadPuzzle("3x3Puzzle.txt")
+
+	// If the puzzle states are not equal, then raise an error
+	if !puzzleStatesEqual(expected, actual) {
+		t.Errorf("Error!\nExpected: %v\nActual:   %v", expected, actual)
+	}
+}
+
+func TestLoadPuzzle2x2(t *testing.T) {
+	// Hardcoded expected value
+	expected := PuzzleState{2, 2,
+		[][]string{
+			{"0", "0"},
+			{"0", "0"}},
+	}
+
+	// Get the actual puzzle from the 3x3Puzzle.txt test file
+	actual := loadPuzzle("2x2Puzzle.txt")
 
 	// If the puzzle states are not equal, then raise an error
 	if !puzzleStatesEqual(expected, actual) {
